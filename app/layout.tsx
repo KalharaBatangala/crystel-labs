5ec68d2819712460eaf0ac8cc1d11de303a63572
Kalhara Batangala <kalharabatangala@gmail.com>
2025-12-29 20:09:00 +0530

    update layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import "./globals.css";
 import Navbar from "@/components/navbar/Navbar";
 import Footer from "@/components/footer/Footer";
 
-
+// Font configurations
 const robotoSlab = Roboto_Slab({
   variable: "--font-roboto-slab",
   subsets: ["latin"],
